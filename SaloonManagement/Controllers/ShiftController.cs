0a3f8edd381648d914aa20e188478c8cf0f19601
yawar2019 <yawarali17@gmail.com>
2020-12-01 23:33:11 +0530

    Update Document By Ali Shift Table

---
+++ SaloonManagement/Controllers/ShiftController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SaloonManagement.Controllers
+{
+    public class ShiftController : Controller
+    {
+        // GET: Shift
+        public ActionResult GetEmployeesShift()
+        {
+            return View();
+        }
+    }
+}
